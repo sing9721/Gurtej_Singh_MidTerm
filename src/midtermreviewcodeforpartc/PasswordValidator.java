0e981abfe323d4e92bb5958df27602b54a2fe84a
sing9721 <sing9721@sheridancollege.ca>
2020-02-19 19:29:34 -0500

    initial commit for new Student code

---
+++ src/midtermreviewcodeforpartc/PasswordValidator.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermreviewcodeforpartc;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gurtej singh
+ */
+public class PasswordValidator {
+   String password;
+    public void RUN(){
+        Scanner sc = new Scanner(System.in);
+        boolean validPassword=false;
+        String password="";
+        while(!validPassword)
+        {
+            System.out.println("Passwords must have at least 8 characters");
+            System.out.println("Passwords must have at least one special character");
+            System.out.println("Please enter your desired password:"); 
+            
+            password = sc.nextLine();
+            int specialCharCount=0;
+            //iterate over each character to see if it is a special character
+            for(int i=0;i<password.length(); i++)
+            {
+                if(!(Character.isLetterOrDigit(password.charAt(i))))
+                {
+                    //now we know there is at least one special character
+                    specialCharCount++;
+                }
+            }
+            if(specialCharCount>0 &&password.length()>7)
+            {
+                validPassword=true;
+            }
+        }//loop only ends when password is valid so now we create the User
+        
+    }
+}
